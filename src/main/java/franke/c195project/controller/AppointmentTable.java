83264d2981ae52f6502396ec17829e8d1c012ad3
Abigail Franke <123238828+AFranke92@users.noreply.github.com>
2026-02-03 15:58:40 +0900

    Update AppointmentTable.java

--- src/main/java/franke/c195project/controller/AppointmentTable.java
+++ src/main/java/franke/c195project/controller/AppointmentTable.java
@@ -27,8 +27,6 @@ import java.util.ResourceBundle;
  * FXML controller class
  * @author
  * Abigail Franke
- * afra480@wgu.edu
- * Student Id: 010025705
  */
 
 public class AppointmentTable implements Initializable {
